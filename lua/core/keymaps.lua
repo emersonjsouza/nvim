1601e50ceb6fc5df3f3975a0092c4def12496ab9
Emerson Jose <emersonjprogramador@gmail.com>
2024-07-23 09:03:15 -0300

    patch: add basic config

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.lastatus = 2
+vim.autowrite =  true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+--use spaces for tabs and whanot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h',':nohlsearch<CR>')
